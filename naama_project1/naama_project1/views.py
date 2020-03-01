8f59819f75c1316229aeb4ef2ac0a9218d472e00
razfranko raz <naama.steklov@gmail.com>
2020-03-01 08:03:50 +0200

    Add data to of studants

--- naama_project1/naama_project1/views.py
+++ naama_project1/naama_project1/views.py
@@ -5,6 +5,42 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from naama_project1 import app
+from naama_project1.Models.LocalDatabaseRoutines import create_LocalDatabaseServiceRoutines
+
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import json 
+import requests
+
+import io
+import base64
+
+from os import path
+
+from flask   import Flask, render_template, flash, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError
+
+
+from naama_project1.Models.QueryFormStructure import QueryFormStructure 
+from naama_project1.Models.QueryFormStructure import LoginFormStructure 
+from naama_project1.Models.QueryFormStructure import UserRegistrationFormStructure 
+
+###from DemoFormProject.Models.LocalDatabaseRoutines import IsUserExist, IsLoginGood, AddNewUser 
+
+db_Functions = create_LocalDatabaseServiceRoutines() 
+
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +71,130 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+
+@app.route('/Album')
+def Album():
+    """Renders the about page."""
+    return render_template(
+        'PictureAlbum.html',
+        title='Pictures',
+        year=datetime.now().year,
+        message='Welcome to my picture album'
+    )
+
+
+@app.route('/Query', methods=['GET', 'POST'])
+def Query():
+
+    Name = None
+    Country = ''
+    capital = ''
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\Data\\capitals.csv'))
+    df = df.set_index('Country')
+
+    raw_data_table = df.to_html(classes = 'table table-hover')
+
+    form = QueryFormStructure(request.form)
+     
+    if (request.method == 'POST' ):
+        name = form.name.data
+        Country = name
+        if (name in df.index):
+            capital = df.loc[name,'Capital']
+            raw_data_table = ""
+        else:
+            capital = name + ', no such country'
+        form.name.data = ''
+
+
+
+    return render_template('Query.html', 
+            form = form, 
+            name = capital, 
+            Country = Country,
+            raw_data_table = raw_data_table,
+            title='Query by the user',
+            year=datetime.now().year,
+            message='This page will use the web forms to get user input'
+        )
+
+# -------------------------------------------------------
+# Register new user page
+# -------------------------------------------------------
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = UserRegistrationFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (not db_Functions.IsUserExist(form.username.data)):
+            db_Functions.AddNewUser(form)
+            db_table = ""
+
+            flash('Thanks for registering new user - '+ form.FirstName.data + " " + form.LastName.data )
+            # Here you should put what to do (or were to go) if registration was good
+        else:
+            flash('Error: User with this Username already exist ! - '+ form.username.data)
+            form = UserRegistrationFormStructure(request.form)
+
+    return render_template(
+        'register.html', 
+        form=form, 
+        title='Register New User',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+# -------------------------------------------------------
+# Login page
+# This page is the filter before the data analysis
+# -------------------------------------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (db_Functions.IsLoginGood(form.username.data, form.password.data)):
+            flash('Login approved!')
+            #return redirect('<were to go if login is good!')
+        else:
+            flash('Error in - Username and/or password')
+   
+    return render_template(
+        'login.html', 
+        form=form, 
+        title='Login to data analysis',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+
+
+@app.route('/dataModel')
+def dataModel():
+    """Renders the contact page."""
+    return render_template(
+        'dataModel.html',
+        title='This is my Data Model page abou UFO',
+        year=datetime.now().year,
+        message='In this page we will display the datasets we are going to use in order to answer ARE THERE UFOs'
+    )
+
+
+@app.route('/DataSet1')
+def DataSet1():
+
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\Data\\capitals.csv'))
+    raw_data_table = df.to_html(classes = 'table table-hover')
+
+
+    """Renders the contact page."""
+    return render_template(
+        'DataSet1.html',
+        title='This is Data Set 1 page',
+        raw_data_table = raw_data_table,
+        year=datetime.now().year,
+        message='In this page we will display the datasets we are going to use in order to answer ARE THERE UFOs'
+    )
+
+
--- naama_project1/runserver.py
+++ naama_project1/runserver.py
@@ -11,4 +11,6 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+
+    app.config['SECRET_KEY'] = 'naama'
     app.run(HOST, PORT)
